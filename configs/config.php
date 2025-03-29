b1a45b6254b9d98a19bae378db435ce076e9848b
Dennisdesign <dengitonga2019@gmail.com>
2025-03-29 10:25:40 +0300

    application update

--- configs/config.php
+++ configs/config.php
@@ -1,9 +1,11 @@
+
 <?php
 
-$host = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$database = "ManageStock"; 
+$host = "sql305.infinityfree.com"; // Get this from InfinityFree MySQL Databases
+$username = "if0_37712493";
+$password = "Moses2339";
+$database = "if0_37712493_managestock";
+
 $mysqli = new mysqli($host, $username, $password, $database);
 
 # Check connection
